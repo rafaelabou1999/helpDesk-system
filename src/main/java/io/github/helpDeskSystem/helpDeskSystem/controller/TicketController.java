f0e6899b4cda0104830b423f96fca26e79e0fe34
Rafaela Bourdette <rafaelabourdette@gmail.com>
2026-05-06 19:40:19 -0300

    Starting to create endpoints for tickets

--- src/main/java/io/github/helpDeskSystem/helpDeskSystem/controller/TicketController.java
+++ src/main/java/io/github/helpDeskSystem/helpDeskSystem/controller/TicketController.java
@@ -1,9 +1,17 @@
 package io.github.helpDeskSystem.helpDeskSystem.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("tickets")
+@RequestMapping("users")
 public class TicketController {
+    @PostMapping("users/{id}/tickets")
+    @Operation(summary = "Create a ticket")
+    public void createTicket(@PathVariable Long id){
+
+    }
 }
